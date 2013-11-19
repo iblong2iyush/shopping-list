087bd055dc3703f35755ed4d070eed2b529206be
Pam Rucinque <prucinque@gmail.com>
2013-11-19 21:28:22 +1100

    Working statically with Node

--- app/public/services/ShoppingListServices.js
+++
@@ -1,8 +0,0 @@
-var shoppingListServices = angular.module('ShoppingListServices', ['ngResource']);
-
-shoppingListServices.factory('ShoppingList', ['$resource',
-  function($resource){
-    return $resource('domain/shoppingList.json', {}, {
-      query: {method:'GET', isArray:true}
-    });
-  }]);
