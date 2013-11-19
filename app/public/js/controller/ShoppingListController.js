7dd78eada08c36afa9d50443b849290ab0686565
Pam Rucinque <prucinque@gmail.com>
2013-11-19 22:37:01 +1100

    Gets the shopping list from the server

--- app/public/js/controller/ShoppingListController.js
+++ app/public/js/controller/ShoppingListController.js
@@ -1,30 +1,11 @@
-controllers.controller('ShoppingListController', function ShoppingListController($scope, ShoppingList){
+controllers.controller('ShoppingListController', function ShoppingListController($scope, $http){
+	$http.get('domain/shoppingList.json').success(function(data) {
+    	$scope.lists = data;
+  	});
 
-	$scope.lists = [{
-		name: 'Test Shopping List',
-		date: '03/11/2013',
-		items: [
-			{
-				item: {
-					name: 'Coriander',
-					unit: 'bunch'
-				},
-				price: null,
-				qty: '1',
-				ready: false
-			},
-			{
-				item: {
-					name: 'Lime',
-					unit: 'units'
-				},
-				price: null,
-				qty: '8',
-				ready: false
-			}
-		]
-	}];
-
-	//$scope.lists = ShoppingList.query();
+	/*ShoppingList.query(function(data){
+		debugger
+		$scope.lists = data	
+	});*/
 	
 });
